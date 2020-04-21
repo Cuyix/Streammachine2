ec505477de831ae6c3be4397a199926323b0fde4
Devin <devinfeiner@gmail.com>
2020-04-21 20:39:32 +0200

    .

--- src/WriteAndReadDataSet.java
+++ src/WriteAndReadDataSet.java
@@ -7,12 +7,12 @@ ins File geschrieben werden.
 
 import java.io.*;
 
-public class WriteAndReadDataSet {
+public class WriteAndReadDataSet implements StreamReadWrite {
     public static void main(String[] args) {
         // three example data sets
         String sensorName = "MyGoodOldSensor"; // does not change
 
-        long timeStamps[] = new long[3];
+        long[] timeStamps = new long[3];
         timeStamps[0] = System.currentTimeMillis();
         timeStamps[1] = timeStamps[0] + 1; // milli sec later
         timeStamps[2] = timeStamps[1] + 1000; // second later
@@ -35,9 +35,12 @@ public class WriteAndReadDataSet {
         values[2] = valueSet;
         valueSet[0] = (float) 0.7;
         valueSet[1] = (float) 1.2;
+    }
 
         // write three data set into a file
         // TODO: your job. use DataOutputStream / FileOutputStream
+        // TODO: Neuer Input nicht Anwendbar - Wie Strukturieren und Umgehen?
+        public void insertData(String input){
         OutputStream os;
         DataOutputStream dos = null;
         try {
@@ -51,54 +54,59 @@ public class WriteAndReadDataSet {
         int numberOfBytesinString = -1;
         try {
             PrintStream ps = new PrintStream(dos);
-            ps.print(sensorName);
-            numberOfBytesinString = sensorName.getBytes().length;
-            for (int i = 0; i < 3; i++) {
+            ps.print(input);
+            numberOfBytesinString = input.getBytes().length;
+/*            for (int i = 0; i < 3; i++) {
                 dos.writeLong(timeStamps[i]);
                 for (int k = 0; k < values[i].length; k++) {
                     dos.writeFloat(values[i][k]);
                 }
             }
+            */
         } catch (IOException ex) {
             System.err.println("Failed to write data to file.");
             System.exit(0);
         }
+       // }
+
+
 
         // read data from file and print to System.out
         // TODO: your job use DataInputStream / FileInputStream
-        InputStream is;
-        DataInputStream dis = null;
-        try {
-            String filename = "Data.txt";
-            is = new FileInputStream(filename);
-            dis = new DataInputStream(is);
-        } catch (FileNotFoundException ex) {
-            System.err.println("Failed to open file.");
-            System.exit(0);
-        }
-        try {
-            byte[] nameBytes = dis.readNBytes(numberOfBytesinString);
-            String t = new String(nameBytes);
-            System.out.println(t);
-            long time = dis.readLong();
-            System.out.println(time);
-            float val = dis.readFloat();
-            System.out.println(val);
-            long time2 = dis.readLong();
-            System.out.println(time2);
-            for (int i = 0; i < 3; i++) {
-                float val2 = dis.readFloat();
-                System.out.println(val2);
+        public String readData(){
+            InputStream is;
+            DataInputStream dis = null;
+            try {
+                String filename = "Data.txt";
+                is = new FileInputStream(filename);
+                dis = new DataInputStream(is);
+            } catch (FileNotFoundException ex) {
+                System.err.println("Failed to open file.");
+                System.exit(0);
             }
-            long time3 = dis.readLong();
-            System.out.println(time3);
-            for (int i = 0; i < 2; i++) {
-                float val3 = dis.readFloat();
-                System.out.println(val3);
+            try {
+                byte[] nameBytes = dis.readNBytes(numberOfBytesinString);
+                String t = new String(nameBytes);
+                System.out.println(t);
+               /* long time = dis.readLong();
+                System.out.println(time);
+                float val = dis.readFloat();
+                System.out.println(val);
+                long time2 = dis.readLong();
+                System.out.println(time2);
+                for (int i = 0; i < 3; i++) {
+                    float val2 = dis.readFloat();
+                    System.out.println(val2);
+                }
+                long time3 = dis.readLong();
+                System.out.println(time3);
+                for (int i = 0; i < 2; i++) {
+                    float val3 = dis.readFloat();
+                    System.out.println(val3);
+                } */
+            } catch (IOException ex) {
+                System.err.println("Failed to read from file.");
+                System.exit(0);
             }
-        } catch (IOException ex) {
-            System.err.println("Failed to read from file.");
-            System.exit(0);
         }
-    }
 }
